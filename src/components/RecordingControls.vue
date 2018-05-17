503319c47c0084a963c254ff2bd0a3c17b6db586
Martin Schoen <schoenm@cip.ifi.lmu.de>
2018-05-17 21:41:34 +0200

    Add authentication logic in RecordingControls

--- src/components/RecordingControls.vue
+++ src/components/RecordingControls.vue
@@ -1,36 +1,62 @@
 <template>
-  <b-row align-h="end">
-    <b-col cols="auto" class="pr-0" v-if="appState.recording">
-      <code class="button-indent">{{counterValue}}</code>
-    </b-col>
-    <b-col cols="auto">
-      <b-button-group>
-        <!-- stop button -->
-        <b-button :disabled="!appState.recording"
-                  @click="stop">
-          <span class="panelIcon stop"></span>
-        </b-button>
-        <!-- record button -->
-        <b-button :pressed="appState.recording"
-                  @click="record">
-          <span class="panelIcon record"></span>
-        </b-button>
-      </b-button-group>
-    </b-col>
-  </b-row>
+  <div>
+    <b-row align-h="end">
+      <b-col cols="auto" class="pr-0" v-if="appState.recording">
+        <code class="button-indent">{{counterValue}}</code>
+      </b-col>
+      <b-col cols="auto">
+        <b-button-group>
+          <!-- stop button -->
+          <b-button :disabled="!appState.recording"
+                    @click="triggerStop">
+            <span class="panelIcon stop"></span>
+          </b-button>
+          <!-- record button -->
+          <b-button :pressed="appState.recording"
+                    @click="triggerRecording">
+            <span class="panelIcon record"></span>
+          </b-button>
+        </b-button-group>
+      </b-col>
+    </b-row>
+
+    <b-modal title="Enter Passphrase"
+             v-model="showPrompt"
+             no-close-on-esc
+             no-close-on-backdrop
+             hide-header-close>
+      <p class="errorMessage" v-if="error">Wrong passphrase. Please try again.</p>
+      <b-form-input v-model="enteredPassphrase" type="password"></b-form-input>
+      <div slot="modal-footer">
+        <b-button variant="secondary" @click="checkPassphrase">Ok</b-button>
+      </div>
+    </b-modal>
+  </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import { SHA512 } from 'jshashes'
 import Util from '@/shared/util.js'
 
 export default {
   name: 'RecordingControls',
   data () {
-    return {}
+    return {
+      // properties
+      correctPassphraseHashed: 'c4ae69280ce2f63b5983bfd58d4e80395b1a33a77d54dca7d3db146db4a701b803e45a088a4fe849d78ec5ad703f8856aea0285bbebbefc5285ceddef4c7d8f7',
+      enteredPassphrase: '',
+
+      // view controls
+      showPrompt: false,
+      error: false,
+      action: 'record',
+
+      // helpers
+      hasher: new SHA512()
+    }
   },
-  computed: mapState({
-    appState: 'appState',
+  computed: {
     clipPosition: function () {
       if (this.appState && this.appState.recording) {
         let now = new Date().getTime()
@@ -40,24 +66,54 @@ export default {
     },
     counterValue: function () {
       return Util.timerFormat(this.clipPosition)
-    }
-  }),
+    },
+    ...mapState({
+      appState: 'appState'
+    })
+  },
   methods: {
-    record: function () {
+    triggerRecording: function () {
       if (!this.appState.recording) {
-        this.$store.dispatch('switchRecordingStatus', 'start').then(null, error => {
-          console.log(error)
-        })
+        this.action = 'record'
+        this.showPrompt = true
       }
     },
-    stop: function () {
+    triggerStop: function () {
       if (this.appState.recording) {
-        this.$store.dispatch('switchRecordingStatus', 'stop').then(_ => {
-          this.$router.push('/recordings')
-        }, error => {
-          console.log(error)
-        })
+        this.action = 'stop'
+        this.showPrompt = true
       }
+    },
+    record: function () {
+      this.$store.dispatch('switchRecordingStatus', 'start').then(null, error => {
+        console.log(error)
+      })
+    },
+    stop: function () {
+      this.$store.dispatch('switchRecordingStatus', 'stop').then(_ => {
+        this.$router.push('/recordings')
+      }, error => {
+        console.log(error)
+      })
+    },
+    checkPassphrase: function () {
+      let enteredPassphraseHashed = this.hasher.hex(this.enteredPassphrase)
+      if (enteredPassphraseHashed === this.correctPassphraseHashed) {
+        switch (this.action) {
+          // Start recording
+          case 'record':
+            this.record()
+            break
+          // stop recording
+          case 'stop':
+          default:
+            this.stop()
+        }
+        this.showPrompt = false
+      } else {
+        this.error = true
+      }
+      this.enteredPassphrase = ''
     }
   }
 }
@@ -90,5 +146,8 @@ export default {
   }
   .active .panelIcon.record {
     animation: 1s ease-in infinite flash;
+  }
+  .errorMessage {
+    color: darkred;
   }
 </style>
